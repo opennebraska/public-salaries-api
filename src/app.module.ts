3969e9961c1c742037cc741cdb25f39e345c1d51
dodgees <dodgees2@gmail.com>
2021-06-06 06:56:01 -0500

    typeorm and db setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {ThrottlerModule} from "@nestjs/throttler";
+import { ThrottlerModule } from '@nestjs/throttler';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [ThrottlerModule.forRoot({ ttl: 60, limit: 100})],
+  imports: [
+    ThrottlerModule.forRoot({ ttl: 60, limit: 100 }),
+    TypeOrmModule.forRoot(typeOrmConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
