f2d36d58ab720dfead5c86982608f3b460059950
dodgees <dodgees2@gmail.com>
2021-06-08 18:39:26 -0500

    Remove agency e2e path

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,12 @@ import { AppService } from './app.service';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
-import { AgencyModule } from './agency/agency.module';
 import { EmployeeModule } from './employee/employee.module';
 
 @Module({
   imports: [
     ThrottlerModule.forRoot({ ttl: 60, limit: 100 }),
     TypeOrmModule.forRoot(typeOrmConfig),
-    AgencyModule,
     EmployeeModule,
   ],
   controllers: [AppController],
