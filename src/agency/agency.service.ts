d8aea0ddf5022a89c711ea0116fb823befff699d
Jay Hannah <jay.hannah@iinteractive.com>
2021-07-27 20:11:17 -0500

    fixes #2: cleanup

--- src/agency/agency.service.ts
+++ src/agency/agency.service.ts
@@ -1,14 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Agency } from './agency.entity';
-//import { Employee } from '../employee/employee.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
 export class AgencyService {
   constructor(
     @InjectRepository(Agency) private agencyRepository: Repository<Agency>,
-    //@InjectRepository(Employee) private employeeRepository: Repository<Employee>,
   ) {}
 
   findById(id: string): Promise<Agency> {
@@ -28,16 +26,11 @@ export class AgencyService {
   async findStats(): Promise<number[]> {
     let queryBuilder = this.agencyRepository.createQueryBuilder('agency');
     const agencies = await queryBuilder.getMany();
-    //console.log(agencies);
     var totalEmployees = 0;
     let agencyCount = agencies.length;
     agencies.forEach(agency => {
       totalEmployees += agency.employeeCount;
     })
-    
-    //const agencyCount = await this.agencyRepository.count();
-    //const employeeCount = await this.employeeRepository.count();
     return [ agencyCount, totalEmployees ];
-    //return [ 17,8 ];
   }
 }
